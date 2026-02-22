5ac816d3780de90c3678364803421082b72a94b9
twisti <denizgebauer@web.de>
2026-02-22 12:34:09 +0100

    Refactor RedisHandlerSuspendInspection to use Kotlin analysis API and improve applicability checks

--- src/main/kotlin/dev/slne/surf/idea/surfideaplugin/redis/inspections/RedisHandlerSuspendInspection.kt
+++ src/main/kotlin/dev/slne/surf/idea/surfideaplugin/redis/inspections/RedisHandlerSuspendInspection.kt
@@ -1,62 +1,69 @@
 package dev.slne.surf.idea.surfideaplugin.redis.inspections
 
-import com.intellij.codeInsight.AnnotationUtil
-import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElementVisitor
+import com.intellij.openapi.util.TextRange
 import dev.slne.surf.idea.surfideaplugin.common.facet.SurfLibraryDetector
 import dev.slne.surf.idea.surfideaplugin.redis.SurfRedisClassNames
-import org.jetbrains.kotlin.asJava.toLightMethods
-import org.jetbrains.kotlin.idea.base.util.module
-import org.jetbrains.kotlin.idea.codeinsight.api.classic.inspections.AbstractKotlinInspection
+import org.jetbrains.kotlin.analysis.api.KaSession
+import org.jetbrains.kotlin.idea.base.psi.KotlinPsiHeuristics
+import org.jetbrains.kotlin.idea.codeinsight.api.applicable.inspections.KotlinApplicableInspectionBase
+import org.jetbrains.kotlin.idea.codeinsight.api.applicators.ApplicabilityRange
+import org.jetbrains.kotlin.idea.codeinsights.impl.base.asQuickFix
+import org.jetbrains.kotlin.idea.quickfix.RemoveModifierFixBase
 import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.psi.KtNamedFunction
-import org.jetbrains.kotlin.psi.KtVisitorVoid
+import org.jetbrains.kotlin.psi.KtVisitor
+import org.jetbrains.kotlin.psi.namedFunctionVisitor
 
-class RedisHandlerSuspendInspection : AbstractKotlinInspection() {
-
+class RedisHandlerSuspendInspection : KotlinApplicableInspectionBase<KtNamedFunction, Unit>() {
     private val handlerAnnotations = setOf(
         SurfRedisClassNames.ON_REDIS_EVENT_ANNOTATION,
         SurfRedisClassNames.HANDLE_REDIS_REQUEST_ANNOTATION
-    )
+    ).map { FqName(it) }
 
     override fun buildVisitor(
         holder: ProblemsHolder,
         isOnTheFly: Boolean
-    ): PsiElementVisitor = object : KtVisitorVoid() {
-        override fun visitNamedFunction(function: KtNamedFunction) {
-            val module = function.module ?: return
-            if (!SurfLibraryDetector.hasSurfRedis(module)) return
-
-            val hasHandlerAnnotation = function.toLightMethods().any {
-                AnnotationUtil.isAnnotated(it, handlerAnnotations, 0)
-            }
-
-            if (!hasHandlerAnnotation) return
-            val suspendModifier = function.modifierList?.getModifier(KtTokens.SUSPEND_KEYWORD)
-
-            if (suspendModifier != null) {
-                holder.registerProblem(
-                    suspendModifier,
-                    "Redis handler '${function.name}' must not be a suspend function. " +
-                            "Use your own coroutineScope.launch {} for async work instead.",
-                    ProblemHighlightType.GENERIC_ERROR,
-                    RemoveSuspendQuickFix()
-                )
-            }
-        }
+    ): KtVisitor<*, *> = namedFunctionVisitor { element ->
+        visitTargetElement(element, holder, isOnTheFly)
     }
 
-    private class RemoveSuspendQuickFix : LocalQuickFix {
-        override fun getName(): String = "Remove 'suspend' modifier"
-        override fun getFamilyName(): String = name
+    override fun isApplicableByPsi(element: KtNamedFunction): Boolean {
+        if (!SurfLibraryDetector.hasSurfRedis(element)) return false
+        if (handlerAnnotations.none { KotlinPsiHeuristics.hasAnnotation(element, it) }) return false
+        if (!element.hasModifier(KtTokens.SUSPEND_KEYWORD)) return false
+        return true
+    }
 
-        override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
-            val function = descriptor.psiElement.parent?.parent as? KtNamedFunction ?: return
-            function.removeModifier(KtTokens.SUSPEND_KEYWORD)
-        }
+    override fun getApplicableRanges(element: KtNamedFunction): List<TextRange> {
+        return ApplicabilityRange.single(element) { it.modifierList?.getModifier(KtTokens.SUSPEND_KEYWORD) ?: it }
+    }
+
+    override fun KaSession.prepareContext(element: KtNamedFunction) {
+    }
+
+    override fun InspectionManager.createProblemDescriptor(
+        element: KtNamedFunction,
+        context: Unit,
+        rangeInElement: TextRange?,
+        onTheFly: Boolean
+    ): ProblemDescriptor {
+        return createProblemDescriptor(
+            element,
+            rangeInElement,
+            "Redis handler '${element.name}' must not be a suspend function. " +
+                    "Use your own coroutineScope.launch {} for async work instead.",
+            ProblemHighlightType.GENERIC_ERROR,
+            onTheFly,
+            RemoveModifierFixBase(
+                element,
+                KtTokens.SUSPEND_KEYWORD,
+                false
+            ).asQuickFix()
+        )
     }
 }
