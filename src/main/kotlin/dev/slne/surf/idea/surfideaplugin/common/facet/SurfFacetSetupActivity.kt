364f1d04e1dee0957b1fcc23e6d760ecdb285f53
twisti <denizgebauer@web.de>
2026-03-01 16:11:03 +0100

    Refactor SurfFacetSetupActivity to streamline facet checks and remove unnecessary logging

--- src/main/kotlin/dev/slne/surf/idea/surfideaplugin/common/facet/SurfFacetSetupActivity.kt
+++ src/main/kotlin/dev/slne/surf/idea/surfideaplugin/common/facet/SurfFacetSetupActivity.kt
@@ -1,7 +1,11 @@
 package dev.slne.surf.idea.surfideaplugin.common.facet
 
-import com.intellij.facet.*
+import com.intellij.facet.Facet
+import com.intellij.facet.FacetConfiguration
+import com.intellij.facet.FacetTypeId
+import com.intellij.facet.FacetTypeRegistry
 import com.intellij.openapi.application.edtWriteAction
+import com.intellij.openapi.application.smartReadAction
 import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider
 import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
 import com.intellij.openapi.module.Module
@@ -14,15 +18,20 @@ import dev.slne.surf.idea.surfideaplugin.surfapi.facet.SurfApiFacetType
 class SurfFacetSetupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
         val modules = project.modules
-
-        println("Modules: ${modules.joinToString { it.name }}")
-
         val modelsProvider = ProjectDataManager.getInstance().createModifiableModelsProvider(project)
+
         try {
             for (module in modules) {
+                val hasSurfApi = smartReadAction(project) {
+                    SurfLibraryDetector.hasSurfApiCore(module)
+                }
+                val hasSurfRedis = smartReadAction(project) {
+                    SurfLibraryDetector.hasSurfRedis(module)
+                }
+
                 context(module, modelsProvider) {
-                    checkSurfApi()
-                    checkSurfRedis()
+                    if (hasSurfApi) ensureFacet(SurfApiFacetType.ID)
+                    if (hasSurfRedis) ensureFacet(SurfRedisFacetType.ID)
                 }
             }
 
@@ -34,40 +43,10 @@ class SurfFacetSetupActivity : ProjectActivity {
                 modelsProvider.dispose()
             }
         }
-
-        for (module in modules) {
-            println(
-                "Module ${module.name} done. (Has surf-redis facet: ${
-                    FacetManager.getInstance(module).getFacetByType(
-                        SurfRedisFacetType.ID
-                    ) != null
-                }, has surf-api facet: ${FacetManager.getInstance(module).getFacetByType(SurfApiFacetType.ID) != null})"
-            )
-        }
     }
 
     context(module: Module, modelsProvider: IdeModifiableModelsProvider)
-    private suspend fun checkSurfApi() {
-        if (SurfLibraryDetector.hasSurfApiCoreSafe(module)) {
-            println("Module ${module.name} has Surf API Core. Ensuring facet...")
-            ensureFacet(
-                SurfApiFacetType.ID
-            )
-        }
-    }
-
-    context(module: Module, modelsProvider: IdeModifiableModelsProvider)
-    private suspend fun checkSurfRedis() {
-        if (SurfLibraryDetector.hasSurfRedisSafe(module)) {
-            println("Module ${module.name} has Surf Redis. Ensuring facet...")
-            ensureFacet(
-                SurfRedisFacetType.ID
-            )
-        }
-    }
-
-    context(module: Module, modelsProvider: IdeModifiableModelsProvider)
-    private suspend fun <F : Facet<C>, C : FacetConfiguration> ensureFacet(typeId: FacetTypeId<F>) {
+    private fun <F : Facet<C>, C : FacetConfiguration> ensureFacet(typeId: FacetTypeId<F>) {
         val facetModel = modelsProvider.getModifiableFacetModel(module)
         val facetType = FacetTypeRegistry.getInstance().findFacetType(typeId)
 
@@ -81,6 +60,5 @@ class SurfFacetSetupActivity : ProjectActivity {
         )
 
         facetModel.addFacet(facet)
-        modelsProvider.commit()
     }
 }
